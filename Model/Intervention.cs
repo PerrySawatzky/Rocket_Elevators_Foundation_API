8edebad00d78e3df29e3082f30efcac7c132bcbe
PerrySawatzky <sawatzky.perry@gmail.com>
2021-07-13 11:55:06 -0400

    added nullable fields to intervention model

--- Model/Intervention.cs
+++ Model/Intervention.cs
@@ -7,14 +7,17 @@ public class Intervention
     public int author { get; set; }
     public int customer_id { get; set; }
     public int building_id { get; set; }
-    public int battery_id { get; set; }
-    public int column_id { get; set; }
-    public int elevator_id { get; set; }
-    public int employee_id { get; set; }
-    public DateTime start_of_intervention { get; set; }
-    public DateTime end_of_intervention { get; set; }
-    public string result { get; set; }
-    public string report { get; set; }
+    public int? battery_id { get; set; }
+    public int? column_id { get; set; }
+    public int? elevator_id { get; set; }
+    public int? employee_id { get; set; }
+    public DateTime? start_of_intervention { get; set; }
+    public DateTime? end_of_intervention { get; set; }
+#nullable enable
+    public string? result { get; set; }
+#nullable enable
+    public string? report { get; set; }
+#nullable disable
     public string status { get; set; }
     public DateTime created_at { get; set; }
     public DateTime updated_at { get; set; }
