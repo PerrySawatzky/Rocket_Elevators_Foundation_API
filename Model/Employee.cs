b2752f99e053a1aa4c744d30e77f23c045ec83f1
Perry Sawatzky <sawatzky.perry@gmail.com>
2021-08-20 13:20:11 -0400

    added employee email to model

--- Model/Employee.cs
+++ Model/Employee.cs
@@ -9,6 +9,7 @@ namespace Rocket_Elevators_Foundation_API.Models
         public int user_id { get; set; }
         public string last_name { get; set; }
         public string first_name { get; set; }
+        public string email { get; set; }
         public string title { get; set; }
         public DateTime created_at { get; set; }
         public DateTime updated_at { get; set; }
